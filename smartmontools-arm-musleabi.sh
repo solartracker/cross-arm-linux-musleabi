400d556dd95bd3776be22136825013a1dc4b74ea
Richard Elwell <229418400@users.noreply.github.com>
2026-01-17 16:56:36 -0500

    fix things

--- smartmontools-arm-musleabi.sh
+++ smartmontools-arm-musleabi.sh
@@ -685,9 +685,29 @@ PKG_ROOT=smartmontools
 export PREFIX="${CROSSBUILD_DIR}"
 export PATH="${CROSSBUILD_DIR}/bin:${CROSSBUILD_DIR}/${TARGET}/bin:${PATH}"
 export HOST=${TARGET}
+export SYSROOT="${PREFIX}/${TARGET}"
 
-STAGEDIR="${CROSSBUILD_DIR}"
-mkdir -p "${STAGEDIR}"
+CROSS_PREFIX=${TARGET}-
+export CC=${CROSS_PREFIX}gcc
+export AR=${CROSS_PREFIX}ar
+export RANLIB=${CROSS_PREFIX}ranlib
+export STRIP=${CROSS_PREFIX}strip
+
+export LDFLAGS="-L${PREFIX}/lib -Wl,--gc-sections"
+export CPPFLAGS="-I${PREFIX}/include -D_GNU_SOURCE"
+export CFLAGS="-O3 -march=armv7-a -mtune=cortex-a9 -marm -mfloat-abi=soft -mabi=aapcs-linux -fomit-frame-pointer -ffunction-sections -fdata-sections -pipe -Wall -fPIC -std=gnu99"
+
+case "${HOST_CPU}" in
+    armv7l)
+        LDD="${SYSROOT}/lib/libc.so --list"
+        ;;
+    *)
+        LDD="ldd"
+        ;;
+esac
+
+#STAGEDIR="${CROSSBUILD_DIR}"
+#mkdir -p "${STAGEDIR}"
 SRC_ROOT="${CROSSBUILD_DIR}/src/${PKG_ROOT}"
 mkdir -p "${SRC_ROOT}"
 
@@ -695,7 +715,7 @@ MAKE="make -j$(grep -c ^processor /proc/cpuinfo)" # parallelism
 #MAKE="make -j1"                                  # one job at a time
 
 export PKG_CONFIG="pkg-config"
-export PKG_CONFIG_LIBDIR="${STAGEDIR}/lib/pkgconfig"
+export PKG_CONFIG_LIBDIR="${PREFIX}/lib/pkgconfig"
 unset PKG_CONFIG_PATH
 
 
